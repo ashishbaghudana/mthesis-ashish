8d96a50e8bdd80daa21b2b86283c05418bf09e2c
Ashish Baghudana <ashish.baghudana26@gmail.com>
2015-06-14 15:05:45 +0200

    Added pubmed.py

---
+++ src/pubmed/pubmed.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup as bs
+import requests
+import xml.etree.ElementTree as ET
+
+class Pubmed(object):
+	def __init__(self, pmid):
+		self.pmid = pmid
+		self.url = "http://www.ncbi.nlm.nih.gov/pubmed/"+str(self.pmid)
+		self.url_xml = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&id="+str(self.pmid)
+		try:
+			r = requests.get(url)
+			r_xml = requests.get()
+			data = r.text
+			soup = bs(data)
+			self.abstract = soup.find("div", {"class": "abstr"}).getText()
+			tree = ET.parse()
+		except ConnectionError:
