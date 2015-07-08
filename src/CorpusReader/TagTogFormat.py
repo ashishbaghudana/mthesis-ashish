434acf07eb7fde8cb6fe4901e403db85464d8ab3
Ashish Baghudana <ashish.baghudana26@gmail.com>
2015-07-08 12:03:24 +0200

    Updated CorpusReader with TagTog compatible documents

--- src/CorpusReader/TagTogFormat.py
+++ src/CorpusReader/TagTogFormat.py
@@ -7,14 +7,30 @@ class TagTogReader(object):
         self.file_location = file_location
         self.documents = {}
         self.symbols = [',', '.', ')', ':', ';', '[', ']']
+        self.json_content = {}
 
     def parse(self):
         data_file = open(self.file_location)
         tempString = ""
+        id = 1
         for line in data_file:
             if str(line).strip().startswith("###MEDLINE"):
                 if tempString!="":
-                    self.documents[pmid.rstrip()]=tempString.lstrip().rstrip()
+                    self.documents[pmid.rstrip()]={}
+                    sentences = sent_tokenize(tempString.lstrip().rstrip())
+                    self.documents[pmid.rstrip()]['abstract']=sentences[0]
+                    self.documents[pmid.rstrip()]['text'] = ' '.join(sentences[1:])
+                    id = 1
+                    json_single={}
+                    json_single['annotatable']={}
+                    json_single['annotatable']['parts']={}
+                    json_single['anncomplete']=False
+                    json_single['sources']=[]
+                    json_single['sources'].append({"name": "MEDLINE", "id": pmid.rstrip(), "url": "null"})
+                    json_single['entities']=entities
+                    json_single['relations']=[]
+                    self.json_content[pmid.rstrip()]=json_single
+                entities = []
                 pmid = line.split(":")[1]
                 tempString = ""
             elif str(line)=='\n':
@@ -24,32 +40,32 @@ class TagTogReader(object):
                 if str(line).split()[0] in self.symbols or tempString.endswith('('):
                     tempString = tempString + tempWord[0]
                 else:
+                    if tempWord[1].startswith("B-"):
+                        classId = "e_"+str(id)
+                        id += 1
+                        entity = {}
+                        entity['classId']=classId
+                        entity['offsets']=[{'start': len(tempString), 'text': tempWord[0]}]
+                        entity['confidence']={'state': "", 'who': ['genia4er'], 'prob': 1.0000}
+                        entities.append(entity)
                     tempString = tempString + ' ' + tempWord[0]
         data_file.close()
 
-    def createJSONContent(self):
+    def createJSON(self):
+        for key in self.json_content:
+            cutoff = len(self.documents[key]['abstract'])
+            for i in range(len(self.json_content[key]['entities'])):
+                if self.json_content[key]['entities'][i]['offsets'][0]['start']>cutoff:
+                    self.json_content[key]['entities'][i]['part']='s2h1'
+                else:
+                    self.json_content[key]['entities'][i]['part']='s1h1'
+            f = open(str(key)+'.json', 'w')
+            f.write(json.dumps(self.json_content[key]))
+            f.close()
 
-        self.json={}
-        self.json['annotatable']={}
-        self.json['annotatable']['parts']={}
-        self.json['anncomplete']=False
-        self.json['sources']=[]
-        self.json['entities']=[]
-        self.json['relations']=[]
-
-        data_file = open(self.file_location)
-        entities = []
-        for line in data_file:
-            if str(line).rstrip().lstrip().startswith("###MEDLINE"):
-                self.json['sources'].append({"name": "MEDLINE", "id": pmid, "url": "null"})
-                for entity in entities:
-                    self.json['entities'].append(entity)
-                pmid = line.split(":")[1]
-
-        return
-
-    def writeHTML(self, key, data):
-        sentences = sent_tokenize(data)
+    def writeHTML(self, key):
+        title = self.documents.get(key)['abstract']
+        abstr = self.documents.get(key)['text']
 
         doctype = "<!DOCTYPE html>\n"
 
@@ -61,7 +77,7 @@ class TagTogReader(object):
         body = "\t<body>\n" \
                "\t\t<article>\n" \
                '\t\t\t<section data-type="title">\n' \
-               '\t\t\t\t<h2 id="s1h1">' + sentences[0] + "</h2>\n" \
+               '\t\t\t\t<h2 id="s1h1">' + title + "</h2>\n" \
                "\t\t\t</section>\n"
 
         abstract = '\t\t\t<section data-type="abstract">\n' \
@@ -70,8 +86,6 @@ class TagTogReader(object):
                    '</h3>\n'
 
         content = '\t\t\t\t<p id = "s2s1p1">'
-
-        text = ' '.join(sentences[1:])
 
         content_close = '<\p>\n'
 
@@ -88,15 +102,17 @@ class TagTogReader(object):
         html_file.write(body)
         html_file.write(abstract)
         html_file.write(content)
-        html_file.write(text)
+        html_file.write(abstr)
         html_file.write(content_close)
         html_file.write(abstract_close)
         html_file.write(body_close)
         html_file.write(html_close)
+        return
+
 
     def convert_to_html(self):
         for pmid in self.documents.keys():
-            self.writeHTML(pmid, self.documents[pmid])
+            self.writeHTML(pmid)
 
     def print_documents(self):
         for pmid  in self.documents.keys():
@@ -104,4 +120,5 @@ class TagTogReader(object):
 
 tagtog = TagTogReader('D:\\Genia4ERtask2.iob2')
 tagtog.parse()
-tagtog.convert_to_html()
+#tagtog.convert_to_html()
+tagtog.createJSON()
