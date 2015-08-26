594e9d911de6afc2f83f24f1b009c3b33e8dc7d4
Ashish Baghudana <ashish.baghudana26@gmail.com>
2015-08-26 10:25:02 +0200

    Added TriggerWords - currently a static ArrayList that holds stemmed trigger words

---
+++ src/BiomedicalTextMiningPipeline/src/main/java/org/rostlab/relna/config/TriggerWords.java
@@ -0,0 +1,9 @@
+package org.rostlab.relna.config;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class TriggerWords {
+	public static final ArrayList<String> triggers = new ArrayList<String>(Arrays.asList("control", "regul", "translat", "transcript", "affect", "effect", "bind"));
+	
+}
