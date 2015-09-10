c76fdf9733dd83bd2fa1bef6db9725755c5d1b88
Ashish Baghudana <ashish.baghudana26@gmail.com>
2015-09-10 13:58:27 +0200

    Updated NameTaggerRecognizer to take command line input.

--- src/relna/src/main/java/org/rostlab/relna/driver/NameTaggerRecognizer.java
+++ src/relna/src/main/java/org/rostlab/relna/driver/NameTaggerRecognizer.java
@@ -3,6 +3,7 @@ package org.rostlab.relna.driver;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -22,6 +23,7 @@ import org.rostlab.relna.writer.JSONFileWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.google.common.io.*;
+import java.util.UUID;
 
 public class NameTaggerRecognizer {
 	
@@ -31,17 +33,22 @@ public class NameTaggerRecognizer {
 		DefaultParser parser = new DefaultParser();
 		Options options = new Options();
 		options.addOption("h", "help", false, "Print usage information.");
-		options.addOption("i", "input", true, "File with the corpus.");
 		options.addOption("o", "output", true, "File to save the final output.");
 		
+		Option input = new Option("i", "input", true, "File with the corpus.");
 		Option readerOption = new Option("r", "reader format", true, "Input file format - txt or iob2");
 		Option writerOption = new Option("w", "writer format", true, "Output file format - anndoc or json");
+		Option string = new Option("s", "string text", true, "Command line input text");
 		
+		input.setRequired(false);
 		readerOption.setRequired(false);
+		string.setRequired(false);
 		writerOption.setRequired(true);
 		
+		options.addOption(input);
 		options.addOption(readerOption);
 		options.addOption(writerOption);
+		options.addOption(string);
 		
 		CommandLine commandLine = null;
 		
@@ -51,8 +58,11 @@ public class NameTaggerRecognizer {
 		String output = null;
 		String readerFormat = null;
 		String writerFormat = null;
+		String commandLineText = null;
 		
 		File tempDir = null;
+		
+		tempDir = Files.createTempDir();
 		
 		try {
 			commandLine = parser.parse(options, args);
@@ -65,19 +75,29 @@ public class NameTaggerRecognizer {
 			return;
 		}
 		
-		if (commandLine.hasOption("i")) {
-			inputCorpus = commandLine.getOptionValue("i");
-		} 
-		else {
-			System.err.println("Please specify the corpus.");
-			return;
-		}
-		
-		if (commandLine.hasOption("r")) {
+		if (commandLine.hasOption("r") && !commandLine.hasOption("i")) {
 			readerFormat = commandLine.getOptionValue("r");
+			if (readerFormat.equals("string")) {
+				if (commandLine.hasOption("s")) {
+					commandLineText = commandLine.getOptionValue("s");
+				}
+				else {
+					System.err.println("Please specify sentence to tag from command line");
+				}
+			}
+			else {
+				System.err.println("Please specify valid parameters, input corpus is not given.");
+			}
+		}
+		else if (commandLine.hasOption("r") && commandLine.hasOption("i")) {
+			readerFormat = commandLine.getOptionValue("r");
+			inputCorpus = commandLine.getOptionValue("i");
+		}
+		else if (commandLine.hasOption("i") && !commandLine.hasOption("r")) {
+			readerFormat = FilenameUtils.getExtension(inputCorpus);
 		}
 		else {
-			readerFormat = FilenameUtils.getExtension(inputCorpus);
+			System.err.println("Please specify at least one of the parameters -i or -r");
 		}
 		
 		if (commandLine.hasOption("o")) {
@@ -100,7 +120,6 @@ public class NameTaggerRecognizer {
 			return;
 		}
 		
-		tempDir = Files.createTempDir();
 		gimliOutput = tempDir.getAbsolutePath() + File.separator + FilenameUtils.removeExtension(FilenameUtils.getBaseName(output)) + ".iob2";
 		
 		if (readerFormat.equals("iob2")) {
@@ -112,23 +131,51 @@ public class NameTaggerRecognizer {
 			NamedEntityRecognizer ner = new NamedEntityRecognizer(inputCorpus, gdep, gimliOutput);
 			ner.tag();
 		}
-		else {
-			try {
-				File tempFile = File.createTempFile(FilenameUtils.removeExtension(inputCorpus), ".iob2");
-				GimliConverter gimliConverter = new GimliConverter(inputCorpus, tempFile.getAbsolutePath());
+		else if (readerFormat.equals("txt")) {
+			File tempFile = new File(tempDir.getAbsolutePath()+File.separator+"tempCorpus"+Long.toString(System.nanoTime())+".iob2");
+			GimliConverter gimliConverter = new GimliConverter(inputCorpus, tempFile.getAbsolutePath());
+			gimliConverter.convert();
+			
+			gdep = tempDir.getAbsolutePath() + File.pathSeparator + "gdep.gz";
+			
+			File gdep_file = new File(gdep);
+			if (gdep_file.exists())
+				gdep_file.delete();
+			
+			NamedEntityRecognizer ner = new NamedEntityRecognizer(FilenameUtils.removeExtension(inputCorpus)+".iob2", gdep, gimliOutput);
+			ner.tag();
+		}
+		else if (readerFormat.equals("string")) {
+			File tempFile;
+			File tempCorpus;
+			try {				
+				tempCorpus = new File(tempDir.getAbsolutePath()+File.separator+"corpus"+Long.toString(System.nanoTime())+".txt");				
+				FileWriter fw = new FileWriter(tempCorpus);
+				
+				fw.write("###MEDLINE:"+UUID.randomUUID().toString().replace("-", "")+"\n");
+				fw.write("Title.\n");
+				fw.write(commandLineText);
+				
+				fw.close();
+				
+				tempFile = new File(tempDir.getAbsolutePath()+File.separator+"tempCorpus"+Long.toString(System.nanoTime())+".iob2");
+				GimliConverter gimliConverter = new GimliConverter(tempCorpus.getAbsolutePath(), tempFile.getAbsolutePath());
 				gimliConverter.convert();
 				
-				gdep = tempDir.getAbsolutePath() + File.pathSeparator + "gdep.gz";
+				gdep = tempDir.getAbsolutePath() + File.separator + "gdep.gz";
 				
 				File gdep_file = new File(gdep);
 				if (gdep_file.exists())
 					gdep_file.delete();
 				
-				NamedEntityRecognizer ner = new NamedEntityRecognizer(FilenameUtils.removeExtension(inputCorpus)+".iob2", gdep, gimliOutput);
+				NamedEntityRecognizer ner = new NamedEntityRecognizer(tempFile.getAbsolutePath(), gdep, gimliOutput);
 				ner.tag();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+		}
+		else {
+			System.err.println("Please specify valid reader format - iob2, txt or string");
 		}
 		
 		GimliParser gimli = new GimliParser();
